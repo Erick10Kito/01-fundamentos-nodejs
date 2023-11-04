51ce5a2dbb06ef5c27aca39693107de14f4bf904
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-11-04 16:06:39 -0300

    Streams - Stream de escrita e transformação - aula 3

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,5 +1,5 @@
 //importação de uma stream de leitura
-import {Readable} from 'node:stream';
+import {Readable, Transform, Writable} from 'node:stream';
 
 class OneToHundredStream extends Readable {
     index = 1;
@@ -8,6 +8,7 @@ class OneToHundredStream extends Readable {
         // esse metodo read vai retornar quais são os dados da Stream
         const i = this.index++ // somando mais um ao index
 
+       setTimeout(()=> {
         if( i > 100) {
             this.push(null)
             // push é o metodo utilizado para uma Readable Stream fornecer informações para quem estiver consumindo ela
@@ -16,6 +17,28 @@ class OneToHundredStream extends Readable {
             const buf = Buffer.from(i.toString()) // converti o i para string pois o Buffer não aceita Numeros e depois converti para buffer
             this.push(buf) 
         }
+       },1000)
+    }
+}
+
+//Stream de escrita:
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback) { // usa obrigatoriamente o metodo write que recebe 3 parametros
+//chunk = pedaço que foi lido pela stream de leitura
+// encoding = é como que a informação esta codificada
+// Callback = é uma função que a stream de escrita precisa chamar quando ela terminou de fazer o que ela precisava fazer com aquela informação
+console.log(Number(chunk.toString())*10) 
+callback() // para encerrar a stream      
+}
+}
+
+
+// Stream de Transformação:
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const  transformed = Number(chunk.toString())*-1 // inverte o sinal do valo passado no chunk
+        callback(null,Buffer.from(String(transformed))) // o primeiro parametro é um possivel, que euando der algum erro ira aparecer esse parametro, e o segundo parametro é o valor transformado
+
     }
 }
 
@@ -23,5 +46,10 @@ class OneToHundredStream extends Readable {
 //DENTRO DE UMA STREAM SÓ É POSSIVEL USAR O TIPO BUFFER, TIPOS PRIMITIVOS NÃO SÃO ACEITOS
 
 
-new OneToHundredStream()
-.pipe(process.stdout) // o pipe(process.stdout faz com que escreva os dados lidos no terminal)
+new OneToHundredStream() // estou lendo com a stream de leitura
+.pipe(new InverseNumberStream) // stream de tranformação, ela le dados da strem de leitura e escreve dados para a stream de escrita, ou seja ela faz os dois
+.pipe(new MultiplyByTenStream) // estou escrevendo com a stream de escrita
+
+
+
+// O codigo acima mostra como eu consigo trabalhar com os dados que estão chegando enquanto ainda tem dados sendo lidos
