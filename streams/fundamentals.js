d469671ecc51614c50a70f5c18c262ee0f7332c4
Erick de Bem Campos <erickcamposbem69@gmail.com>
2024-03-21 11:50:38 -0300

    Stream de escrita e transformação

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,7 +1,10 @@
 // process.stdin.pipe(process.stdout)
 
-import {Readable} from 'node:stream'
+import { write } from 'node:fs';
+import {Readable, Transform, Writable} from 'node:stream'
 
+
+//Strem de Leitura
 class OneToHundredStream extends Readable {
     index = 1;
 
@@ -20,4 +23,35 @@ class OneToHundredStream extends Readable {
     }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+
+// Stream de Escrita
+class MultiplyByTenStrem extends Writable {
+    _write(chunk, encoding, callback) {
+        //chunk = pedaço que a gente leu da stream
+        // encoding = como a informação ta codificada
+        // callback = função que stream de escrita chama quando ela terminou de fazer o que ela precisava fazer dentro da operação
+
+        console.log(Number(chunk.toString())*10)
+        callback()
+
+    }
+}
+
+
+
+// Stream de Transformação
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) *-1
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+
+//  O que aconteceu aqui é que o MultiplyByTen esta pegando os dados que estão sendo lidos pela
+//  OneToHundredStream e multiplicando por 10 assim que eles vão chegando,
+//   ate que a stream de leitura termine de fazer toda a leitura
+
+new OneToHundredStream()
+.pipe(new InverseNumberStream())
+.pipe(new MultiplyByTenStrem())
